1f7b41518dd318b83473d06dc13414db5edeea38
Juan Pablo Espinoza <espinosidro1@gmail.com>
2018-10-23 19:51:16 -0300

    work on the core and the board

--- board.py
+++ board.py
@@ -1,16 +1,36 @@
-import PyQt5.QtWidgets as qt
-import sys
+import tkinter
 
-if __name__ == '__main__':
-    
-    app = qt.QApplication(sys.argv)
+master = tkinter.Tk()
 
-    w = qt.QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
+size = 500
+divisions = 8
 
-    qt.QStylePainter
-    
-    sys.exit(app.exec_())
+w = tkinter.Canvas(master, width=size, height=size)
+w.pack()
+
+for i in range(divisions):
+    w.create_line(0, (size / divisions) * i, size, (size / divisions) * i)
+    w.create_line((size/divisions) * i, 0, (size/divisions)*i, size )
+
+def click(event):
+    #absolute position
+    column, row = -1,-1
+    #print("Cursor at X: " + str(event.x) + ", Y: " + str(event.y))
+
+    #column
+    for i in range(divisions):
+        if(event.x > (size/divisions) * i and event.x < (size/divisions) * (i + 1)):
+            #print("Column: " + str(i))
+            column = i
+
+    #row
+    for i in range(divisions):
+        if(event.y > (size/divisions) * i and event.y < (size/divisions) * (i+1)):
+            #print("Row: " + str(i))
+            row = i
+
+    print("Column: " + str(column) + " - Row: " + str(row))
+
+w.bind("<Button-1>", click)
+
+tkinter.mainloop()
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-import PyQt5
+from enum import Enum
+
+class Type:
+    def __init__(self, moves, attacks):
+        self.moves = moves
+        self.attacks = attacks
+        pass
+
+class Types(Enum):
+    king = Type.__init__(1,2)
+
+class Position:
+    def __init__(self, column, row):
+        self.column = column
+        self.row = row
 
 class Piece:
-    def __init__(self, movement, position, type):
-        self.movement = movement
-        self.position = position
+    def __init__(self, type, position):
+        position.__init__(self, position)
         self.type = type
 
