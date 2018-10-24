f16a09db0ea129e69d6c918d37cae4809d40bfe2
Juan Pablo Espinoza <espinosidro1@gmail.com>
2018-10-23 22:39:26 -0300

    finished the class spec

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from enum import Enum
 
 class Type:
-    def __init__(self, moves, attacks):
-        self.moves = moves
-        self.attacks = attacks
-        pass
+    def __init__(self, movements, attacks = None):
+        self.movements = movements
+        if (attacks == None):
+            self.attacks = movements
 
 class Types(Enum):
-    pass
-
-class Position:
-    def __init__(self, column, row):
-        self.column = column
-        self.row = row
+    pawn = Type([[0,1]], [[1,1], [-1,1]])
 
 class Piece:
-    def __init__(self, type, position):
-        position.__init__(self, position)
+    def __init__(self, column, row, type):
+        self.column = column
+        self.row = row
         self.type = type
+
+#declare pieces like this
+pawn1 = Piece(1,0, Types.pawn)
+
